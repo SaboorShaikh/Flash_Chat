b184f67e372c3a2b8118519bbc47a5a4de658414
Abdul Saboor <abdulsaboor.bscssef20@iba-suk.edu.pk>
2025-04-20 19:11:16 +0500

    navigation routes

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flash Chat',
+      debugShowCheckedModeBanner: false,
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
